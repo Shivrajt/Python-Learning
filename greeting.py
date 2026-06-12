769349ab0939f03385bbe12156772ad98243d68c
Shivraj Toor <Shivrajt92@gmail.com>
2026-06-11 23:41:33 -0700

    Add greeting program with input validation

---
+++ greeting.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+name = input("What is your name? ")
+print(f"Nice to meet you, {name}!") 
+
+while True:
+    try:
+        hours = int(input("How many hours do you game? "))
+        break
+    except ValueError:
+        print("Please enter a valid number, dork! ")
+
+if hours > 7:
+    print(f"Wow {hours}?! That's more than an hour a day, Nerd! ")
+    
+elif hours ==7:
+    print(f"Wow so an hour a day, I guess that's aight! Mediocre! ")
+    
+else:
+    print(f"{hours}?! {hours}?!?!?!?! That's it?!? Step it up loser! Sheesh ")
+
+
+    
